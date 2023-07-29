4e68aa361c0cc5051320e7cbd625eca8a9ae9a86
Rakibul Hasan <rakibul.eeekuet@gmail.com>
2023-07-29 12:47:11 +1000

    before re-organising codes

---
+++ utils/plot.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import scienceplots
+
+def plot(x, y, y2=None, xlabel=None, ylabel=None, legend=[], save=False, filename=None):
+    """Plot data points"""
+    plt.style.use(['science'])
+    fig, ax = plt.subplots(1,1)
+    
+    ax.plot(x,y)
+    if y2 is not None:
+        ax.plot(x, y2)
+    
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.legend(legend)
+    
+    if save:
+        plt.savefig(fname=filename+'.pdf', format = 'pdf', bbox_inches='tight')
+        print(f"Saved as {filename}.pdf")
+        
+    fig.show()
