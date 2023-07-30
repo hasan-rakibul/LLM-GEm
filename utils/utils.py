1fbc0011275207e4b1c7ae43bac4c1bb3e55ae2b
Rakibul Hasan <rakibul.eeekuet@gmail.com>
2023-07-30 20:40:12 +1000

    roberta implemented

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import scienceplots
 
+import torch
+import numpy as np
+
 def plot(x, y, y2=None, xlabel=None, ylabel=None, legend=[], save=False, filename=None):
     """Plot data points"""
     plt.style.use(['science'])
@@ -19,3 +22,11 @@ def plot(x, y, y2=None, xlabel=None, ylabel=None, legend=[], save=False, filenam
         print(f"Saved as {filename}.pdf")
         
     fig.show()
+
+
+def get_device(device_id=0):
+    return torch.device("cuda", device_id) if torch.cuda.is_available() else torch.device("cpu")
+
+def set_all_seeds(seed):
+    torch.manual_seed(seed)
+    np.random.seed(seed)
